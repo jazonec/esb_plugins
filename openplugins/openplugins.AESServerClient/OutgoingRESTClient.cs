5fede36a7821002efe9b9a978b6693b7ef87e4a9
dubinskii_sl <dubinskii_sl@vsmpo-avisma.ru>
2022-05-04 12:07:44 +0300

    Initialize

---
+++ openplugins/openplugins.AESServerClient/OutgoingRESTClient.cs
@@ -0,0 +1,21 @@
+﻿using ESB_ConnectionPoints.PluginsInterfaces;
+using Newtonsoft.Json.Linq;
+
+namespace openplugins.AESServerClient
+{
+    internal class OutgoingRESTClient : IOutgoingConnectionPoint
+    {
+        private JObject settings;
+        private IServiceLocator serviceLocator;
+
+        public OutgoingRESTClient(JObject settings, IServiceLocator serviceLocator)
+        {
+            this.settings = settings;
+            this.serviceLocator = serviceLocator;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
