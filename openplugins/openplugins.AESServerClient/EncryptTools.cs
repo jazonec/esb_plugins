7be5d238c299b5e5e1fbd0a99c6087a6460dfdca
jazonec <sldubinsky@yandex.ru>
2022-05-04 13:28:02 +0300

    Сборка исходящей точки
    Реализована исходящая точка, осуществляющая кодирование сообщения из шины и отправку в указанный в настройках endpoint

---
+++ openplugins/openplugins.AESServerClient/EncryptTools.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace openplugins.AESServerClient
+{
+    internal class EncryptTools
+    {
+        private readonly RSA _publicKey = null;
+        private readonly RSA _privateKey = null;
+        private readonly RSAEncryptionPadding _padding;
+
+        public EncryptTools(JObject settings)
+        {
+            _padding = RSAEncryptionPadding.Pkcs1;
+
+            if (settings.ContainsKey("certificate"))
+            {
+                X509Certificate2 _RSACert = new X509Certificate2((string)settings["certificate"]["file"]);
+                _publicKey = _RSACert.GetRSAPublicKey();
+                _privateKey = _RSACert.GetRSAPrivateKey();
+            }
+            else if (settings.ContainsKey("public"))
+            {
+                var publicSettings = (JObject)settings["public"];
+                if (publicSettings.ContainsKey("xml"))
+                {
+                    _publicKey.FromXmlString((string)publicSettings["xml"]);
+                }
+                else if (publicSettings.ContainsKey("modulus"))
+                {
+                    RSAParameters param = new RSAParameters
+                    {
+                        Modulus = Convert.FromBase64String((string)publicSettings["modulus"]),
+                        Exponent = new byte[] { 1, 0, 1 }
+                    };
+                    _publicKey = RSA.Create(param);
+                }
+            }
+        }
+
+        public byte[] Encrypt_RSA(byte[] plainText)
+        {
+            return _publicKey.Encrypt(plainText, _padding);
+        }
+
+        public byte[] Decrypt_RSA(byte[] ciperText)
+        {
+            return _privateKey.Decrypt(ciperText, _padding);
+        }
+
+        public static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key)
+        {
+            byte[] IV = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
+        public static byte[] EncryptStringToBytes_Aes(string plainText, byte[] key)
+        {
+            byte[] IV = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        private static readonly Random random = new Random();
+
+        public static byte[] RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return Encoding.UTF8.GetBytes(new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray()));
+        }
+    }
+}
--- openplugins/openplugins.AESServerClient/OutgoingRESTClient.cs
+++ openplugins/openplugins.AESServerClient/OutgoingRESTClient.cs
@@ -1,21 +1,181 @@
 ﻿using ESB_ConnectionPoints.PluginsInterfaces;
 using Newtonsoft.Json.Linq;
+using System;
+using RestSharp;
+using RestSharp.Serializers;
+using System.Threading;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace openplugins.AESServerClient
 {
     internal class OutgoingRESTClient : IOutgoingConnectionPoint
     {
-        private JObject settings;
-        private IServiceLocator serviceLocator;
+        // main settings
+        private readonly ILogger _logger;
+        private readonly bool _debugMode;
+        private readonly bool _encode;
+
+        // endpoint
+        private readonly EndpointSettings _endpointSettings;
+
+        // RSA
+        private readonly EncryptTools _encryptTools;
 
         public OutgoingRESTClient(JObject settings, IServiceLocator serviceLocator)
         {
-            this.settings = settings;
-            this.serviceLocator = serviceLocator;
+            if (!settings.ContainsKey("endpoint"))
+            {
+                throw new ArgumentNullException("endpoint");
+            }
+            if (!settings.ContainsKey("rsa"))
+            {
+                throw new ArgumentNullException("rsa");
+            }
+
+            _logger = serviceLocator.GetLogger(GetType());
+            _debugMode = (bool)settings["debug"];
+            _encode = (bool)settings["encode"];
+
+            _endpointSettings = new EndpointSettings((JObject)settings["endpoint"]);
+            _encryptTools = new EncryptTools((JObject)settings["rsa"]);
+        }
+
+        public void Cleanup()
+        {
         }
 
         public void Dispose()
         {
+        }
+
+        public void Initialize()
+        {
+        }
+        public void Run(IMessageSource messageSource, IMessageReplyHandler replyHandler, CancellationToken ct)
+        {
+            while (!ct.IsCancellationRequested)
+            {
+                Message message = messageSource.PeekLockMessage(ct, 1000);
+                if (message != null)
+                {
+                    JObject o = new JObject();
+                    o["id"] = message.Id.ToString();
+
+                    if (_encode)
+                    {
+                        byte[] key = EncryptTools.RandomString(16);
+                        o["file"] = Convert.ToBase64String(EncryptTools.EncryptStringToBytes_Aes(Encoding.UTF8.GetString(message.Body), key));
+                        o["keyhash"] = _encryptTools.Encrypt_RSA(key);
+                    }
+                    else
+                    {
+                        o["file"] = Convert.ToBase64String(message.Body);
+                        o["keyhash"] = "";
+                    }
+                    WriteLogString(o.ToString());
+                    try
+                    {
+                        SendMessageToEndpoint(o);
+                        messageSource.CompletePeekLock(message.Id);
+                    }
+                    catch (WebException ex)
+                    {
+                        _logger.Error("Ошибка выполнения запроса!", ex);
+                        messageSource.AbandonPeekLock(message.Id);
+                    }
+                    catch (InvalidDataException ex)
+                    {
+                        _logger.Error("Ошибка передачи данных в эндпоинт!", ex);
+                        messageSource.CompletePeekLock(message.Id, MessageHandlingError.RejectedMessage, ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Error("Ошибка выполнения запроса!", ex);
+                        messageSource.CompletePeekLock(message.Id, MessageHandlingError.UnknowError, ex.Message);
+                    }
+                }
+                else
+                {
+                    ct.WaitHandle.WaitOne(new TimeSpan(0, 0, 30));
+                }
+            }
+        }
+
+        private void SendMessageToEndpoint(JObject message)
+        {
+            using (RestClient client = new RestClient(_endpointSettings.Url))
+            {
+                RestRequest request = new RestRequest(_endpointSettings.Url, _endpointSettings.Method)
+                    .AddHeader("login", _endpointSettings.Username)
+                    .AddHeader("key", _endpointSettings.Password)
+                    .AddHeader("Content-Encoding", "UTF-8")
+                    .AddStringBody(message.ToString(), ContentType.Json);
+
+                var resTask = client.ExecuteAsync(request);
+                resTask.Wait();
+                var response = resTask.Result;
+                if (resTask.Status == TaskStatus.Faulted)
+                {
+                    throw new WebException(resTask.Exception.Message);
+                }
+                else if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new InvalidDataException(Encoding.Unicode.GetString(Encoding.Unicode.GetBytes(response.Content)));
+                }
+            }
+        }
+
+        private void WriteLogString(string log)
+        {
+            if (_debugMode)
+            {
+                _logger.Debug(log);
+            }
+        }
+
+    }
+    internal class EndpointSettings
+    {
+        private string _url;
+        private string _port;
+        private string _username;
+        private string _password;
+        private Method _method;
+
+        public string Url { get { return _url; } set { _url = value; } }
+        public string Port { get { return _port; } set { _port = value; } }
+        public string Username { get { return _username; } set { _username = value; } }
+        public string Password { get { return _password; } set { _password = value; } }
+        public Method Method { get { return _method; } set { _method = value; } }
+
+        public EndpointSettings(JObject settings)
+        {
+            _url = (string)settings["url"];
+            _port = (string)settings["port"];
+            _username = (string)settings["username"];
+            _password = (string)settings["password"];
+            string methodString = (string)settings["method"];
+            switch (methodString)
+            {
+                case "POST":
+                    _method = Method.Post;
+                    break;
+                case "PUT":
+                    _method = Method.Put;
+                    break;
+                case "DELETE":
+                    _method = Method.Delete;
+                    break;
+                case "GET":
+                    _method = Method.Get;
+                    break;
+                default:
+                    _method = Method.Post;
+                    break;
+            }
         }
     }
 }
