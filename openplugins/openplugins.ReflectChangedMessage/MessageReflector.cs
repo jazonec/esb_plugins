745d8e85f13b738387cb7f311987bbbdc21db753
jazonec <sldubinsky@yandex.ru>
2022-05-12 15:45:18 +0300

    Добавлено хранение hash в redis или mongo
    Реализовано "в лоб", не по канонам ооп. Но работает...
    Дополнительная плюшка - версия плагина в PluginInfo.xml увеличивается автоматически при успешной сборке

--- openplugins/openplugins.ReflectChangedMessage/MessageReflector.cs
+++ openplugins/openplugins.ReflectChangedMessage/MessageReflector.cs
@@ -2,11 +2,15 @@
 using Enyim.Caching.Configuration;
 using Enyim.Caching.Memcached;
 using ESB_ConnectionPoints.PluginsInterfaces;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
 using Newtonsoft.Json.Linq;
 using StackExchange.Redis;
 using System;
 using System.Text;
 using System.Threading;
+using System.Linq;
 
 namespace openplugins.ReflectChangedMessage
 {
@@ -15,6 +19,7 @@ namespace openplugins.ReflectChangedMessage
         private readonly ILogger _logger;
         private readonly IMessageFactory _messageFactory;
         private readonly bool _debugMode;
+        private MessageHashChecker hashChecker;
 
         public void Cleanup()
         {
@@ -22,6 +27,7 @@ namespace openplugins.ReflectChangedMessage
 
         public void Dispose()
         {
+            hashChecker.Dispose();
         }
 
         public void Initialize()
@@ -37,7 +43,15 @@ namespace openplugins.ReflectChangedMessage
 
         public void Run(IMessageSource messageSource, IMessageReplyHandler replyHandler, CancellationToken ct)
         {
+            /*if (_debugMode)
+            {
+                WriteLogString("One minute pause...");
+                ct.WaitHandle.WaitOne(60000);
+            }*/
+
+            hashChecker = new MessageHashChecker();
             _logger.Info(string.Format("Приступил к работе {0}", DateTime.Now.ToString()));
+            _logger.Info(string.Format("Режим хранения HASH: {0}", hashChecker.GetMode().ToString()));
             while (!ct.IsCancellationRequested)
             {
                 try
@@ -49,20 +63,18 @@ namespace openplugins.ReflectChangedMessage
                         ct.WaitHandle.WaitOne(30000);
                         continue;
                     }
-                    using (MessageHashChecker hashChecker = new MessageHashChecker(message))
+                    hashChecker.SetNewMessage(message);
+                    if (hashChecker.IsChanged())
                     {
-                        if (hashChecker.IsChanged())
-                        {
-                            WriteLogString(string.Format("Reflect changed object, id={0}, hash={1}", message.GetPropertyValue<string>("Id"), hashChecker.Hash));
-                            SendChangedMessage(message, replyHandler, ct);
-                            hashChecker.SaveHash();
-                        }
-                        else
-                        {
-                            WriteLogString(string.Format("Object not change, id={0}, hash={1}", message.GetPropertyValue<string>("Id"), hashChecker.Hash));
-                        }
-                        messageSource.CompletePeekLock(message.Id);
+                        WriteLogString(string.Format("Reflect changed object, id={0}, hash={1}", message.GetPropertyValue<string>("Id"), hashChecker.Hash));
+                        SendChangedMessage(message, replyHandler, ct);
+                        hashChecker.SaveHash();
                     }
+                    else
+                    {
+                        WriteLogString(string.Format("Object not change, id={0}, hash={1}", message.GetPropertyValue<string>("Id"), hashChecker.Hash));
+                    }
+                    messageSource.CompletePeekLock(message.Id);
                 }
                 catch (Exception ex)
                 {
@@ -91,29 +103,33 @@ namespace openplugins.ReflectChangedMessage
     }
     internal class MessageHashChecker : IDisposable
     {
-        private readonly MemcachedClient _client;
+        private readonly MemcachedClient _memcach;
 
         private readonly IDatabase _redisDB;
         private readonly ConnectionMultiplexer _redis;
 
+        private readonly MongoClient _mongo;
+        private readonly IMongoDatabase _mongoDB;
+        IMongoCollection<MessageHashMongo> _hashCollection;
+
         private readonly HashCheckerMode _mode;
+        private string messageId;
+        private string type;
+        private string hash;
 
-        private readonly string messageId;
-        private readonly string type;
-
-        public string Hash { get; }
+        public string Hash { get => hash; set => hash = value; }
+        public HashCheckerMode GetMode()
+        {
+            return _mode;
+        }
 
         public void Dispose()
         {
-            _client?.Dispose();
+            _memcach?.Dispose();
             _redis?.Dispose();
         }
-        public MessageHashChecker(Message message)
+        public MessageHashChecker()
         {
-            messageId = message.GetPropertyValue<string>("Id");
-            type = message.Type;
-            Hash = Encoding.UTF8.GetString(message.Body).GetHashCode().ToString();
-
             try
             {
                 _redis = ConnectionMultiplexer.Connect("localhost");
@@ -128,10 +144,22 @@ namespace openplugins.ReflectChangedMessage
 
             try
             {
+                _mongo = new MongoClient("mongodb://localhost:27017");
+                _mongoDB = _mongo.GetDatabase("HASH_TABLE");
+                _mode = HashCheckerMode.MongoDB;
+                return;
+            }
+            catch
+            {
+                // и mongo нет
+            }
+
+            try
+            {
                 MemcachedClientConfiguration memConfig;
                 memConfig = new MemcachedClientConfiguration();
                 memConfig.AddServer("localhost:11211");
-                _client = new MemcachedClient(memConfig);
+                _memcach = new MemcachedClient(memConfig);
                 _mode = HashCheckerMode.MemCached;
                 return;
             }
@@ -140,28 +168,44 @@ namespace openplugins.ReflectChangedMessage
                 // этого нет...
             }
 
-            throw new NotImplementedException("Вариант mongoDB не реализован");
+            throw new NotImplementedException("Варианты закончились...");
+        }
+
+        public void SetNewMessage(Message message)
+        {
+            messageId = message.GetPropertyValue<string>("Id");
+            type = message.Type;
+            Hash = Encoding.UTF8.GetString(message.Body).GetHashCode().ToString();
         }
 
         public bool IsChanged()
         {
-            string currentHash = GetCurrentHash();
+            string currentHash = GetCurrentHashAsync();
             return currentHash != Hash;
         }
         public void SaveHash()
         {
             SetCurrentHash();
         }
-        private string GetCurrentHash()
+        private string GetCurrentHashAsync()
         {
             switch (_mode)
             {
                 case HashCheckerMode.Redis:
                     return _redisDB.StringGet(type + messageId);
                 case HashCheckerMode.MemCached:
-                    return _client.Get<string>(type + messageId);
+                    return _memcach.Get<string>(type + messageId);
                 case HashCheckerMode.MongoDB:
-                    throw new NotImplementedException();
+                    _hashCollection = _mongoDB.GetCollection<MessageHashMongo>(type + "_message_hash");
+                    try
+                    {
+                        MessageHashMongo ff = _hashCollection.Find(x => x.Id == messageId).Single();
+                        return ff.Hash;
+                    }
+                    catch
+                    {
+                        return null;
+                    }
             }
             return null;
         }
@@ -173,18 +217,41 @@ namespace openplugins.ReflectChangedMessage
                     _redisDB.StringSet(type + messageId, Hash);
                     break;
                 case HashCheckerMode.MemCached:
-                    _client.Store(StoreMode.Set, type + messageId, Hash);
+                    _memcach.Store(StoreMode.Set, type + messageId, Hash);
                     break;
                 case HashCheckerMode.MongoDB:
-                    throw new NotImplementedException();
+                    _hashCollection = _mongoDB.GetCollection<MessageHashMongo>(type + "_message_hash");
+                    _hashCollection.ReplaceOne(
+                        x => x.Id == messageId,
+                        new MessageHashMongo(messageId, type, Hash),
+                        new UpdateOptions() { IsUpsert = true});
+                    break;
             }
         }
     }
-
     internal enum HashCheckerMode
     {
         MemCached,
         Redis,
         MongoDB
+    }
+    internal class MessageHashMongo
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; }
+        [BsonElement("Type")]
+        [BsonRepresentation(BsonType.String)]
+        public string Type { get; set; }
+        [BsonElement("Hash")]
+        [BsonRepresentation(BsonType.String)]
+        public string Hash { get; set; }
+
+        public MessageHashMongo(string id, string type, string hash)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+            Hash = hash ?? throw new ArgumentNullException(nameof(hash));
+        }
     }
 }
