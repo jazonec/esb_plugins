2598b320491f94d8c0058911d97f4fe335e2bcc6
jazonec <sldubinsky@yandex.ru>
2022-07-08 16:53:00 +0300

    Рефакторинг, подготовка к версии 2
    Немного привел в порядок код, добавил отладочных параметров.

--- openplugins/openplugins.ADIntegration/ADObjectsIngoing.cs
+++ openplugins/openplugins.ADIntegration/ADObjectsIngoing.cs
@@ -16,26 +16,44 @@ namespace openplugins.ADIntegration
         private readonly ILogger _logger;
         private readonly IMessageFactory _messageFactory;
         private readonly bool _debugMode;
+        private readonly bool _debugUsers;
+        private readonly bool _debugGroups;
 
         private readonly int _delayMinutes;
 
+        private readonly DirectoryEntry _de;
         private readonly string _adUser;
         private readonly string _adPwd;
         private readonly string _adPath;
 
-        private readonly string[] _adFields;
+        private readonly string[] _adUserFields;
+        private readonly bool _usersWithGroups;
+
+        private readonly string[] _adGroupFields;
+        private readonly bool _groupWithMembers;
 
         public ADObjectsIngoing(JObject settings, IServiceLocator serviceLocator)
         {
             _logger = serviceLocator.GetLogger(GetType());
             _messageFactory = serviceLocator.GetMessageFactory();
+
             _debugMode = (bool)settings["debug"];
+            _debugUsers = (bool)settings["debugusers"];
+            _debugGroups = (bool)settings["debuggroups"];
+
             _adUser = (string)settings["user"];
             _adPwd = (string)settings["password"];
             _adPath = (string)settings["path"];
-            //string fields = "objectSid,objectGuid," + (string)settings["fields"];
-            string fields = (string)settings["fields"];
-            _adFields = fields.Split(',');
+            _de = new DirectoryEntry("LDAP://" + _adPath, _adUser, _adPwd);
+
+            string userFields = "objectSid,objectGuid,useraccountcontrol," + (string)settings["userfields"];
+            _adUserFields = userFields.Split(',');
+            _usersWithGroups = (bool)settings["addgroupstouser"];
+
+            string groupFields = (string)settings["groupfields"];
+            _adGroupFields = groupFields.Split(',');
+            _groupWithMembers = (bool)settings["addmemberstogroups"];
+
             _delayMinutes = (int)settings["delay"] != 0 ? (int)settings["delay"] : 3600;
         }
 
@@ -72,90 +90,183 @@ namespace openplugins.ADIntegration
                 _oldTime = DateTime.Now.AddMinutes(_delayMinutes);
                 WriteLogString(string.Format("Next tick {0}", _oldTime.ToString()));
 
-                SendADUsersToESB(messageHandler, ct);
+                if (!_debugMode || (_debugMode && _debugUsers))
+                {
+                    SendADUsersToESB(messageHandler, ct);
+                }
+                if (!_debugMode || (_debugMode && _debugGroups))
+                {
+                    SendADGroupsToESB(messageHandler, ct);
+                }
+
+                WriteLogString("Done!");
             }
+        }
+
+        private void SendADGroupsToESB(IMessageHandler messageHandler, CancellationToken ct)
+        {
+            using (DirectorySearcher ds = new DirectorySearcher(_de, "(&(objectCategory=group))"))
+            {
+                ds.PageSize = 400;
+
+                SearchResultCollection results = ds.FindAll();
+
+                foreach (SearchResult sr in results)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        return; // выходим если шина пытается остановить адаптер
+                    }
+
+                    JObject _mesObj = new JObject
+                    {
+                        { "guid", GetAdObjectGuid(sr) },
+                        { "sid", GetAdObjectSid(sr) },
+                        { "whenchanged", ((DateTime)sr.Properties["whenChanged"][0]).ToString("s") }
+                    };
+
+                    foreach (string key in _adGroupFields)
+                    {
+                        if (key == "objectSid" || key == "objectGuid" || key == "whenChanged")
+                        {
+                            continue;
+                        }
+                        _mesObj.Add(key, sr.Properties[key].Count == 0 ? "" : sr.Properties[key][0].ToString());
+                    }
+
+                    if (_groupWithMembers)
+                    {
+                        _mesObj.Add("members", GetGroupsMembers(sr.GetDirectoryEntry()));
+                    }
+
+                    SendToESB(_mesObj, "ADGroup", messageHandler, ct);
+                }
+            }
+        }
+
+        private JObject GetGroupsMembers(DirectoryEntry objGroupEntry)
+        {
+            JArray _member = new JArray();
+            JObject _members = new JObject();
+            foreach (object objMember in objGroupEntry.Properties["member"])
+            {
+                _member.Add(objMember.ToString());
+            }
+            _members.Add("member", _member);
+            return _members;
         }
 
         private void SendADUsersToESB(IMessageHandler messageHandler, CancellationToken ct)
         {
-            DirectoryEntry de = new DirectoryEntry("LDAP://" + _adPath, _adUser, _adPwd);
-            //DirectorySearcher ds = new DirectorySearcher(de, "(&(objectCategory=User)(objectClass=person))", _adFields);
-            DirectorySearcher ds = new DirectorySearcher(de, "(&(objectCategory=User)(objectClass=person))");
-            ds.PageSize = 400;
-
-            SearchResultCollection results = ds.FindAll();
-
-            foreach (SearchResult sr in results)
+            using (DirectorySearcher ds = new DirectorySearcher(_de, "(&(objectCategory=User)(objectClass=person))", _adUserFields))
             {
-                if (sr.Properties["samAccountName"].Count == 0) continue;
+                ds.PageSize = 400;
+                SearchResultCollection results = ds.FindAll();
 
-                JObject _mesObj = new JObject();
-
-                using (PrincipalContext context = new PrincipalContext(ContextType.Domain, _adPath, _adUser, _adPwd))
+                foreach (SearchResult sr in results)
                 {
-                    UserPrincipal user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, sr.Properties["samAccountName"][0].ToString());
-                    _mesObj.Add("sid", user.Sid.ToString());
-                    _mesObj.Add("guid", user.Guid.ToString());
-                    _mesObj.Add("enabled", user.Enabled);
-                    //
-                }
-
-                /*
-                string sidStr;
-                string uuidStr;
-
-                if (sr.Properties["objectSid"].Count == 0)
-                {
-                    WriteLogString("Отсутствует SID! " + sr.Path);
-                    continue;
-                }
-                else
-                {
-                    byte[] sid = (byte[])sr.Properties["objectSid"][0];
-                    sidStr = new SecurityIdentifier(sid, 0).ToString();
-                }
-
-                if (sr.Properties["objectGuid"].Count == 0)
-                {
-                    WriteLogString("Отсутствует Guid! " + sr.Path);
-                    continue;
-                }
-                else
-                {
-                    byte[] uuid = (byte[])sr.Properties["objectGuid"][0];
-                    uuidStr = new Guid(uuid).ToString();
-                }
-                */
-
-                foreach (string key in _adFields)
-                {
-                    /*if (key == "objectSid")
+                    if (ct.IsCancellationRequested)
                     {
-                        _mesObj.Add("sid", sidStr);
-                        continue;
+                        return; // выходим если шина пытается остановить адаптер
                     }
-                    if (key == "objectGuid") {
-                        _mesObj.Add("guid", uuidStr);
-                        continue;
-                    }*/
-                    _mesObj.Add(key, sr.Properties[key].Count == 0 ? "" : sr.Properties[key][0].ToString());
-                }
 
-                Message mes = _messageFactory.CreateMessage("ADRecord");
-                mes.Body = Encoding.UTF8.GetBytes(_mesObj.ToString());
-                while (true)
-                {
-                    bool result = messageHandler.HandleMessage(mes);
-                    if (result)
+                    if (sr.Properties["samAccountName"].Count == 0 ||
+                        sr.Properties["objectSid"].Count == 0 ||
+                        sr.Properties["objectGuid"].Count == 0
+                       )
                     {
-                        break;
+                        WriteLogString("Отсутствуют обязательные данные (один из samAccountName, SID, Guid)! " + sr.Path);
+                        continue; // неполноценный объект в домене? Пусть админы посмотрят
                     }
-                    ct.WaitHandle.WaitOne(1000);
+
+                    JObject _mesObj = new JObject
+                    {
+                        { "sid", GetAdObjectSid(sr) },
+                        { "guid", GetAdObjectGuid(sr) },
+                        { "enabled", !Convert.ToBoolean((int)sr.Properties["useraccountcontrol"][0] & 0x0002) },
+                        { "whenchanged", ((DateTime)sr.Properties["whenChanged"][0]).ToString("s") }
+                    };
+
+                    foreach (string key in _adUserFields)
+                    {
+                        if (key == "objectSid" || key == "objectGuid" || key == "userAccountСontrol" || key == "whenChanged")
+                        {
+                            continue;
+                        }
+                        _mesObj.Add(key, sr.Properties[key].Count == 0 ? "" : sr.Properties[key][0].ToString());
+                    }
+
+                    if (_usersWithGroups)
+                    {
+                        JObject _groups = GetUserGroups(sr.Properties["samAccountName"][0].ToString(), ct);
+                        _mesObj.Add("groups", _groups);
+                    }
+
+                    SendToESB(_mesObj, "ADUser", messageHandler, ct);
+
                 }
             }
-            ds.Dispose();
         }
 
+        private string GetAdObjectGuid(SearchResult sr)
+        {
+            byte[] uuid = (byte[])sr.Properties["objectGuid"][0];
+            return new Guid(uuid).ToString();
+        }
+
+        private string GetAdObjectSid(SearchResult sr)
+        {
+            byte[] sid = (byte[])sr.Properties["objectSid"][0];
+            return new SecurityIdentifier(sid, 0).ToString();
+        }
+
+        private JObject GetUserGroups(string samAccountName, CancellationToken ct)
+        {
+            JObject _groups = new JObject();
+
+            using (PrincipalContext context = new PrincipalContext(ContextType.Domain, _adPath, _adUser, _adPwd))
+            {
+                UserPrincipal user = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, samAccountName);
+
+                PrincipalSearchResult<Principal> groups = user.GetAuthorizationGroups();
+                JArray _groupLine = new JArray();
+                foreach (Principal grp in groups)
+                {
+                    if (!ct.IsCancellationRequested)
+                    {
+                        JObject _group = new JObject
+                        {
+                            { "guid", grp.Guid },
+                            { "sid", grp.Sid.ToString() },
+                            { "name", grp.Name },
+                            { "description", grp.Description },
+                            { "samaccountname", grp.SamAccountName },
+                            { "distinguishedname", grp.DistinguishedName }
+                        };
+                        _groupLine.Add(_group);
+                    }
+                    else
+                        return new JObject();
+                }
+                _groups.Add("group", _groupLine);
+            }
+            return _groups;
+        }
+
+        private void SendToESB(JObject objectToSend, string objectType, IMessageHandler messageHandler, CancellationToken ct)
+        {
+            Message mes = _messageFactory.CreateMessage(objectType);
+            mes.Body = Encoding.UTF8.GetBytes(objectToSend.ToString());
+            while (true)
+            {
+                bool result = messageHandler.HandleMessage(mes);
+                if (result)
+                {
+                    break;
+                }
+                ct.WaitHandle.WaitOne(1000);
+            }
+        }
         private void WriteLogString(string log)
         {
             if (_debugMode)
