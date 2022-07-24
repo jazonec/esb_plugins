81c79235dafc408a3be85755fb411b02be63f8e4
jazonec <sldubinsky@yandex.ru>
2022-07-24 22:10:33 +0300

    первая версия подписчика Учимся работать с AMQ

---
+++ openplugins/openplugins.ActiveMQ/ActiveMQConsumer_ESB.cs
@@ -0,0 +1,75 @@
+﻿using ESB_ConnectionPoints.PluginsInterfaces;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace openplugins.ActiveMQ
+{
+    internal delegate void MessageReceivedDelegate(string message);
+    internal class ActiveMQConsumer_ESB : IStandartIngoingConnectionPoint
+    {
+        private readonly ILogger _logger;
+        private readonly IMessageFactory _messageFactory;
+        private IMessageHandler _messageHandler;
+        private readonly bool _debugMode;
+        private readonly string _queueName;
+        private readonly string _host;
+        private readonly string _login;
+        private readonly string _password;
+
+        public ActiveMQConsumer_ESB(JObject settings, IServiceLocator serviceLocator)
+        {
+            _logger = serviceLocator.GetLogger(GetType());
+            _messageFactory = serviceLocator.GetMessageFactory();
+            _debugMode = (bool)settings["debug"];
+
+            _queueName = (string)settings["queue"];
+            _host = (string)settings["host"];
+            _login = (string)settings["login"];
+            _password = (string)settings["password"];
+        }
+        public void Cleanup()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void Run(IMessageHandler messageHandler, CancellationToken ct)
+        {
+            if (_debugMode)
+            {
+                WriteLogString("30 секунд до старта!");
+                ct.WaitHandle.WaitOne(30000);
+            }
+
+            WriteLogString("Приступаю к инициализации подписчика к очереди " + _queueName);
+            using (TopicConsumer topicConsumer = new TopicConsumer(_queueName, _host, _login, _password, "ActiveMQConsumer_ESB", "ActiveMQConsumer_ESB"))
+            {
+                topicConsumer.OnMessageReceived += new MessageReceivedDelegate(SendMessagetoESB);
+                WriteLogString("Подписчик инициализиолван");
+                while (!ct.IsCancellationRequested)
+                {
+                    ct.WaitHandle.WaitOne(5000);
+                }
+            }
+        }
+
+        private void SendMessagetoESB(string activeMQmessage)
+        {
+            WriteLogString("Получено сообщение: " + activeMQmessage);
+        }
+
+        private void WriteLogString(string log)
+        {
+            if (_debugMode)
+            {
+                _logger.Debug(log);
+            }
+        }
+    }
+}
---
+++ openplugins/openplugins.ActiveMQ/TopicConsumer.cs
@@ -0,0 +1,48 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+using System;
+
+namespace openplugins.ActiveMQ
+{
+    internal class TopicConsumer : IDisposable
+    {
+        private readonly string topicName = null;
+        private readonly IConnectionFactory connectionFactory;
+        private readonly IConnection connection;
+        private readonly ISession session;
+        private readonly IMessageConsumer consumer;
+        private bool isDisposed = false;
+
+        public TopicConsumer(string topicName, string brokerUri, string userName, string password, string clientId, string consumerId)
+        {
+            this.topicName = topicName;
+            connectionFactory = new ConnectionFactory(brokerUri);
+            connection = connectionFactory.CreateConnection(userName, password);
+            connection.ClientId = clientId;
+            connection.Start();
+            session = connection.CreateSession();
+            ActiveMQTopic topic = new ActiveMQTopic(topicName);
+            consumer = session.CreateDurableConsumer(topic, consumerId, "2 > 1", false);
+            consumer.Listener += new MessageListener(OnMessage);
+        }
+
+        private void OnMessage(IMessage message)
+        {
+            ITextMessage textMessage = message as ITextMessage;
+            OnMessageReceived?.Invoke(textMessage.Text);
+        }
+
+        public event MessageReceivedDelegate OnMessageReceived;
+        public void Dispose()
+        {
+            if (!isDisposed)
+            {
+                consumer.Dispose();
+                session.Dispose();
+                connection.Dispose();
+                isDisposed = true;
+            }
+        }
+    }
+}
