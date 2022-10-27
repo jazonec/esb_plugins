2b6bc783eb7b82d60a4be92f4339c935db35d288
Дубинский Сергей <dubinskii_sl@vsmpo-avisma.ru>
2022-10-27 13:32:07 +0500

    Добавлена фабрика для исходящей точки

---
+++ openplugins/openplugins.ADIntegration/ADObjectsCreator.cs
@@ -0,0 +1,40 @@
+﻿using ESB_ConnectionPoints.PluginsInterfaces;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace openplugins.ADIntegration
+{
+    internal class ADObjectsCreator : IStandartOutgoingConnectionPoint
+    {
+        private readonly ILogger _logger;
+        private readonly IMessageFactory _messageFactory;
+        private readonly bool _debugMode;
+
+        public ADObjectsCreator(JObject settings, IServiceLocator serviceLocator)
+        {
+            _logger = serviceLocator.GetLogger(GetType());
+            _debugMode = (bool)settings["debug"];
+            _messageFactory = serviceLocator.GetMessageFactory();
+        }
+
+        public void Cleanup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Run(IMessageSource messageSource, IMessageReplyHandler replyHandler, CancellationToken ct)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
